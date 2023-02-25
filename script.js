e96d27031b62f19f205c446bcd64382cd5c20804
Nsohan <sohan5538@gmail.com>
2023-02-25 18:53:50 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let step1 = document.getElementById("step1");
+let step2 = document.getElementById("step2");
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+
+
+
+// Init()
+
+// function Init(){
+//     step2.style.visibility="hidden";
+    
+
+// }
+
+// btn1.addEventListener("click",function(){
+   
+//     step1.style.visibility="hidden";
+//     step2.style.visibility="visible";
+// })
+
+// btn2.addEventListener("click",function(){
+//     step1.style.visibility="visible";
+//     step2.style.visibility="hidden";
+   
+// })
+
